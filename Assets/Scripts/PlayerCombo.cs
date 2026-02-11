8359323ef76d6cd766aaef8f671a2d45324bfc0a
Ricardo <rp_mira@hotmail.com>
2026-02-11 10:26:48 -0300

    loop no combos ok

--- Assets/Scripts/PlayerCombo.cs
+++ Assets/Scripts/PlayerCombo.cs
@@ -42,37 +42,42 @@ public class PlayerCombo : MonoBehaviour
     {
         for(int i = 0; i < combos.Length; i++)  // Percorre todos combos
         {
-            if (Input.GetButtonDown(combos[i].hits[currentCombo.Count].inputButton))  // Verifica se esta pressionando botão do hit correspondente
+            if (combos[i].hits.Length >= currentCombo.Count)     // Filtro para ele olhar apenas os que for igual ou maior na quantidade de hits. Caso tenha menos ele nem perde tempo em olhar ja vai para proximo
             {
-                if(currentCombo.Count == 0)     // Verifica se é o primeiro 
+                if (Input.GetButtonDown(combos[i].hits[currentCombo.Count].inputButton))  // Verifica se esta pressionando botão do hit correspondente
                 {
-                    Debug.Log("Primeiro hit foi adicionado");        // Log
-                    PlayHit(combos[i].hits[currentCombo.Count]);     // Chama função para executar animação do hit
-                    break;                                           // Finaliza loop
-                }
-                else
-                {
-                    bool comboMatch = false;                        // Controla se sequencia de combo esta igual ou não
-                    for(int y = 0; y < currentCombo.Count; y++)     // Percorre todo combo por outro index
+                    if(currentCombo.Count == 0)     // Verifica se é o primeiro 
                     {
-                        if(currentCombo[y] != combos[i].hits[y].inputButton)    // Verifica se na lista de combos o Combo i no hit y são diferentes
-                        {
-                            Debug.Log("Input não pertence ao hit atual");   // Log
-                            comboMatch = false;                             // Sinaliza que não é uma combinação valida
-                            break;                                          // Finaliza loop
-                        }
-                        else
-                        {
-                            comboMatch = true;  // Sinaliza que combinação é valida
-                        }
+                        Debug.Log("Primeiro hit foi adicionado");        // Log
+                        PlayHit(combos[i].hits[currentCombo.Count]);     // Chama função para executar animação do hit
+                        break;                                           // Finaliza loop
                     }
+                    else
+                    {
+                        bool comboMatch = false;                        // Controla se sequencia de combo esta igual ou não
+                        for(int y = 0; y < currentCombo.Count; y++)     // Percorre todo combo por outro index
+                        {
+                            if(currentCombo[y] != combos[i].hits[y].inputButton)    // Verifica se na lista de combos o Combo i no hit y são diferentes
+                            {
+                                Debug.Log("Input não pertence ao hit atual");   // Log
+                                comboMatch = false;                             // Sinaliza que não é uma combinação valida
+                                break;                                          // Finaliza loop
+                            }
+                            else
+                            {
+                                comboMatch = true;  // Sinaliza que combinação é valida
+                            }
+                        }
 
-                    if (comboMatch) // Verifica se comboMatch é verdadeiro
-                    {
-                        PlayHit(combos[i].hits[currentCombo.Count]);    // Executa animação do combo i do ultimo hit
+                        if (comboMatch) // Verifica se comboMatch é verdadeiro
+                        {
+                            Debug.Log("Hit adicionado ao combo");           // Log
+                            PlayHit(combos[i].hits[currentCombo.Count]);    // Executa animação do combo i do ultimo hit
+                            break;                                          // Sai do loop
+                        }
                     }
+                    
                 }
-                
             }
         }
     }
